af8f5c03733dbdb5f09aab767ca446c74b45f48a
Camila Villalba <cami_vp_23@hotmail.com>
2024-09-24 14:28:55 +0200

    execute validarClave function in main

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,63 @@
 import "./style.css";
 
-console.log((120-18) / 6);
+import { validarClave } from "./motor";
 
+const commonPasswords: string[] = [
+  "password",
+  "123456",
+  "qwerty",
+  "admin",
+  "letmein",
+  "welcome",
+  "monkey",
+  "sunshine",
+  "password1",
+  "123456789",
+  "football",
+  "iloveyou",
+  "1234567",
+  "123123",
+  "12345678",
+  "abc123",
+  "qwerty123",
+  "1q2w3e4r",
+  "baseball",
+  "password123",
+  "superman",
+  "987654321",
+  "mypass",
+  "trustno1",
+  "hello123",
+  "dragon",
+  "1234",
+  "555555",
+  "loveme",
+  "hello",
+  "hockey",
+  "letmein123",
+  "welcome123",
+  "mustang",
+  "shadow",
+  "12345",
+  "passw0rd",
+  "abcdef",
+  "123abc",
+  "football123",
+  "master",
+  "jordan23",
+  "access",
+  "flower",
+  "qwertyuiop",
+  "admin123",
+  "iloveyou123",
+  "welcome1",
+  "monkey123",
+  "sunshine1",
+  "password12",
+  "1234567890",
+];
+
+const nombreUsuario = "camila";
+const clave = "CaM1l@789!";
+
+console.log(validarClave(nombreUsuario, clave, commonPasswords));
